fd2668a8b054b46b2853da15dfa2f22a131b55ff
Đỗ Huy Hoàng <subaen255@gmail.com>
2025-02-07 13:12:38 +0700

    Tạo Seed

---
+++ WebBanGiayOnline/Data/Seed.cs
@@ -0,0 +1,19 @@
+﻿using ClssLib;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebBanGiay.Data
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder builder)
+        {
+            List<Vai_Tro> vai_Tros = new List<Vai_Tro>()
+            {
+                new Vai_Tro{ID= Guid.NewGuid(),ten_vai_tro = "Admin",trang_thai = 1,ngay_tao = DateTime.Now},
+                new Vai_Tro{ID= Guid.NewGuid(),ten_vai_tro = "Nhân Viên",trang_thai = 1,ngay_tao = DateTime.Now},
+                new Vai_Tro{ID= Guid.NewGuid(),ten_vai_tro = "Khách Hàng",trang_thai = 1,ngay_tao = DateTime.Now}
+            };
+            builder.Entity<Vai_Tro>().HasData(vai_Tros);
+        }
+    }
+}
