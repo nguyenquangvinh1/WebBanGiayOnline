8bc5db2e16817d192092584b6a8e8fe59f11c542
Huyentrang136 <ocxinh24@gmail.com>
2025-05-28 16:34:33 +0700

    vinh

---
+++ WebBanGiayOnline/Models/HoaDonVM.cs
@@ -0,0 +1,13 @@
+﻿using ClssLib;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebBanGiay.Models
+{
+    
+    public class TTHD
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+    
+}
---
+++ WebBanGiayOnline/Models/ViewModel/LoginViewmodel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebBanGiay.Models.ViewModel
+{
+    public class LoginViewmodel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
